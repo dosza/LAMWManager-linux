ec83af17556bbc8ac4f327e1479e23dc6d104222
Daniel <oliveira.daniel109@gmail.com>
2018-08-17 15:00:07 -0300

    Full clean old cfg
    Esta versão remove configurações residuais com o comando clean

--- lamw-install.sh
+++ lamw-install.sh
@@ -3,11 +3,11 @@
 #Universidade federal de Mato Grosso
 #Curso ciencia da computação
 #AUTOR: Daniel Oliveira Souza <oliveira.daniel@gmail.com>
-#Versao LAMW-INSTALL: 0.0.5
+#Versao LAMW-INSTALL: 0.0.6
 #Descrição: Este script configura o ambiente de desenvolvimento para o LAMW
 
 #pwd
-LAMW_INSTALL_VERSION="0.0.5"
+LAMW_INSTALL_VERSION="0.0.6"
 LAMW_INSTALL_WELCOME=(
 	"\t\tWelcome LAMW-Install version: $LAMW_INSTALL_VERSION\n"
 	"\t\tPowerd by DanielTimelord\n"
@@ -142,24 +142,60 @@ CleanOldConfig(){
 		sudo rm  -r $HOME/LazarusAndroid
 	fi
 	if [ -e $LAZANDROID_HOME ] ; then
-			sudo rm $LAZANDROID_HOME -r
-		fi
+		sudo rm $LAZANDROID_HOME -r
+	fi
 
-		if [ -e $ANDROID_HOME ] ; then
-			sudo rm $ANDROID_HOME  -r
-		fi
+	if [ -e $ANDROID_HOME ] ; then
+		sudo rm $ANDROID_HOME  -r
+	fi
 
-		if [ -e $HOME/.laz4android ] ; then
-			rm -r $HOME/.laz4android
-		fi
-		if [ -e $HOME/.local/share/applications/laz4android.desktop ];then
-			rm $HOME/.local/share/applications/laz4android.desktop
-		fi
+	if [ -e $HOME/.laz4android ] ; then
+		rm -r $HOME/.laz4android
+	fi
+	if [ -e $HOME/.local/share/applications/laz4android.desktop ];then
+		rm $HOME/.local/share/applications/laz4android.desktop
+	fi
 
-		if [ -e $HOME/.gradle ]; then
-			rm -r $HOME/.gradle
-		fi
-		
+	if [ -e $HOME/.gradle ]; then
+		rm -r $HOME/.gradle
+	fi
+	if [ -e /usr/lib/fpc ] ; then
+		sudo rm /usr/lib/fpc -r 
+		sudo rm /etc/fpc* 
+	fi
+	if [ -e /usr/local/lib/fpc ]; then
+		sudo rm /usr/local/lib/fpc -r 
+	fi
+	if [ /usr/bin/ppcrossarm ]; then
+		sudo rm /usr/bin/ppc* 
+	fi
+	if [  /usr/bin/fpc ]; then
+		sudo rm /usr/bin/fpc* 
+	fi
+	if [ -e usr/bin/arm-embedded-as ] ; then    
+		sudo rm usr/bin/arm-embedded-as
+	fi
+	if [ -e  /usr/bin/arm-linux-androideabi-ld ]; then
+		 sudo rm /usr/bin/arm-linux-androideabi-ld
+	fi
+	if [ -e /usr/bin/arm-embedded-ld  ]; then
+		sudo /usr/bin/arm-embedded-ld           
+	fi 
+	if [ -e /usr/bin/arm-linux-as ] ; then 
+	 	sudo rm  /usr/bin/arm-linux-as
+	fi
+
+
+	if [ -e /usr/bin/arm-linux-androideabi-as ]; then
+		sudo rm /usr/bin/arm-linux-androideabi-as
+	fi
+	if [ -e /usr/bin/arm-linux-ld ] ; then 
+		sudo rm /usr/bin/arm-linux-ld
+	fi
+	sed -i '/export PATH=$PATH:$HOME\/android\/ndk-toolchain/d'  $HOME/.bashrc #\/ is scape of /
+	sed -i '/export PATH=$PATH:$HOME\/android\/gradle-4.1\/bin/d' $HOME/.bashrc
+	sed -i '/export PATH=$PATH:$HOME\/android\/ndk-toolchain/d'  $HOME/.profile
+	sed -i '/export PATH=$PATH:$HOME\/android\/gradle-4.1\/bin/d' $HOME/.profile		
 }
 SearchPackage(){
 	index=-1
@@ -303,40 +339,6 @@ mainInstall(){
 			sudo apt-get install $libs_android $prog_tools  -y --allow-unauthenticated --fix-missing
 		fi
 
-
-		# SearchPackage fpc
-		# index=$?
-		# parseFPC ${packs[$index]}
-		# fpc_cfg_str=(
-		# 	"#IFDEF ANDROID"
-		# 	"#IFDEF CPUARM"
-		# 	"-XParm-linux-androideabi-"
-		# 	"-Fl$ANDROID_HOME/ndk/platforms/android-28/arch-arm/usr/lib"
-		# 	"-FD$ANDROID_HOME/ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
-		# 	'-Fu/usr/lib/fpc/$fpcversion/units/$fpctarget'
-		# 	'-Fu/usr/lib/fpc/$fpcversion/units/$fpctarget/*'
-		# 	'-Fu/usr/lib/fpc/$fpcversion/units/$fpctarget/rtl'
-		# 	"#ENDIF"
-		# 	"#ENDIF"
-		# )
-
-		# if [ -e $FPC_CFG_PATH ] ; then  # se exiir /etc/fpc.cfg
-		# 	fpc_cfg_teste=$(cat $FPC_CFG_PATH) # abre /etc/fpc.cfg
-		# 	flag_fpc_cfg=0 # flag da sub string de configuração"
-		# 	case "$fpc_cfg_teste" in 
-		# 		*"#IFDEF ANDROID"*)
-		# 		flag_fpc_cfg=1
-		# 		;;
-		# 	esac
-
-		# 	if [ $flag_fpc_cfg != 1 ]; then # caso o arquvo ainda não esteja configurado
-		# 		for ((i = 0 ; i<${#fpc_cfg_str[@]};i++)) 
-		# 		do
-		# 			#echo ${fpc_cfg_str[i]}
-		# 			sudo echo "${fpc_cfg_str[i]}" | sudo tee -a  $FPC_CFG_PATH
-		# 		done	
-		# 	fi
-		# fi
 		configureFPC
 		if [ $USE_PROXY = 1 ] ; then
 			ActiveProxy 1
@@ -498,259 +500,25 @@ LAMW-Install (Linux supported Debian 9, Ubuntu 16.04 LTS, Linux Mint 18)
 Generate Laz4LAMW to  android-sdk=$SDK_VERSION"
 
 case "$1" in
-	"cfg-fpc")
-		if [ "$(whoami)" = "root" ]
-		then
-			echo "executando em mode root"
-			configureFPC $2
-		fi
-	;;
+
 	"clean")
 		CleanOldConfig
-
-		# if [ -e $LAZANDROID_HOME ] ; then
-		# 	sudo rm $LAZANDROID_HOME -r
-		# fi
-
-		# if [ -e $ANDROID_HOME ] ; then
-		# 	sudo rm $ANDROID_HOME  -r
-		# fi
-
-		# if [ -e $HOME/.laz4android ] ; then
-		# 	rm -r $HOME/.laz4android
-		# fi
-		# if [ -e $HOME/.local/share/applications/laz4android.desktop ];then
-		# 	rm $HOME/.local/share/applications/laz4android.desktop
-		# fi
-
-		# if [ -e $HOME/.gradle ]; then
-		# 	rm -r $HOME/.gradle
-		# fi
-		
-	#sudo rm /usr/src/fpcsrc -r
 	;;
 	"install")
-		mainInstall $2
-		# 	if [ "$2" = "--use_proxy" ] ;then
-		# 		USE_PROXY=1
-		# 	fi
-
-		# 	if [ $USE_PROXY = 1 ]; then
-		# 		SDK_MANAGER_CMD_PARAMETERS=("platforms;android-25" "build-tools;25.0.3" "tools" "ndk-bundle" "extras;android;m2repository" --no_https --proxy=http --proxy_host=$PROXY_SERVER --proxy_port=$PORT_SERVER )
-		# 		SDK_LICENSES_PARAMETERS=( "--licenses" --no_https --proxy=http --proxy_host=$PROXY_SERVER --proxy_port=$PORT_SERVER)
-		# 		export http_proxy=$PROXY_URL
-		# 		export https_proxy=$PROXY_URL
-		# #	ActiveProxy 1
-		# 	else
-		# 		SDK_MANAGER_CMD_PARAMETERS=("platforms;android-25" "build-tools;25.0.3" "tools" "ndk-bundle" "extras;android;m2repository")			#ActiveProxy 0
-		# 		SDK_LICENSES_PARAMETERS=(--licenses )
-		# 	fi
-		# 	sudo apt update;
-		# 	sudo apt-get remove --purge fpc* -y
-		# 	sudo apt-get remove --purge  lazarus* -y
-		# 	sudo apt-get autoremove --purge -y
-		# 	#sudo apt-get install fpc
-
-
-
-
-		# sudo apt-get install $libs_android $prog_tools  -y --allow-unauthenticated
-		# if [ "$?" != "0" ]; then
-		# 	sudo apt-get install $libs_android $prog_tools  -y --allow-unauthenticated --fix-missing
-		# fi
-
-
-		# # SearchPackage fpc
-		# # index=$?
-		# # parseFPC ${packs[$index]}
-		# # fpc_cfg_str=(
-		# # 	"#IFDEF ANDROID"
-		# # 	"#IFDEF CPUARM"
-		# # 	"-XParm-linux-androideabi-"
-		# # 	"-Fl$ANDROID_HOME/ndk/platforms/android-28/arch-arm/usr/lib"
-		# # 	"-FD$ANDROID_HOME/ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
-		# # 	'-Fu/usr/lib/fpc/$fpcversion/units/$fpctarget'
-		# # 	'-Fu/usr/lib/fpc/$fpcversion/units/$fpctarget/*'
-		# # 	'-Fu/usr/lib/fpc/$fpcversion/units/$fpctarget/rtl'
-		# # 	"#ENDIF"
-		# # 	"#ENDIF"
-		# # )
-
-		# # if [ -e $FPC_CFG_PATH ] ; then  # se exiir /etc/fpc.cfg
-		# # 	fpc_cfg_teste=$(cat $FPC_CFG_PATH) # abre /etc/fpc.cfg
-		# # 	flag_fpc_cfg=0 # flag da sub string de configuração"
-		# # 	case "$fpc_cfg_teste" in 
-		# # 		*"#IFDEF ANDROID"*)
-		# # 		flag_fpc_cfg=1
-		# # 		;;
-		# # 	esac
-
-		# # 	if [ $flag_fpc_cfg != 1 ]; then # caso o arquvo ainda não esteja configurado
-		# # 		for ((i = 0 ; i<${#fpc_cfg_str[@]};i++)) 
-		# # 		do
-		# # 			#echo ${fpc_cfg_str[i]}
-		# # 			sudo echo "${fpc_cfg_str[i]}" | sudo tee -a  $FPC_CFG_PATH
-		# # 		done	
-		# # 	fi
-		# # fi
-		# configureFPC
-		# if [ $USE_PROXY = 1 ] ; then
-		# 	ActiveProxy 1
-		# else
-		# 	ActiveProxy 0
-		# fi
-		# mkdir -p $ANDROID_SDK
-
-		# changeDirectory $ANDROID_HOME
-		# wget "https://services.gradle.org/distributions/gradle-4.1-bin.zip"
-		# unzip "gradle-4.1-bin.zip"
-		# rm "gradle-4.1-bin.zip"
-		# changeDirectory $ANDROID_SDK
-		# #echo "pwd=$PWD"
-		# #ls -la 
-		# wget "https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip" #getting sdk 
-		# unzip sdk-tools-linux-3859397.zip
-		# rm sdk-tools-linux-3859397.zip
-
-		# changeDirectory $ANDROID_SDK/tools/bin #change directory
-		# #sed -i 's/exec "$JAVACMD" "$@"/yes | exec "$JAVACMD" "$@"/g' sdkmanager #modifica o sdkmanager adicionando pipe para aceitar a licença
-		# #Install SDK packages and NDK
-		# #./sdkmanager "platforms;android-25" "build-tools;25.0.3" "tools" "ndk-bundle" "extras;android;m2repository"
-		# #$SDK_MANAGER_CMD
-		# #ls
-		
-		# if [ $flag_new_ubuntu_lts = 1 ]
-		# then
-		# 	sudo apt-get remove --purge openjdk-9-* -y 
-		# 	sudo apt-get remove --purge openjdk-11* -y
-		# fi
-		# yes | ./sdkmanager ${SDK_LICENSES_PARAMETERS[*]}
-		# ./sdkmanager ${SDK_MANAGER_CMD_PARAMETERS[*]}  # instala sdk sem intervenção humana  
-		# #sed -i 's/yes | exec "$JAVACMD" "$@"/exec "$JAVACMD" "$@"/g' sdkmanager # restaura o estado anterior do arquivo
-
-		# ln -sf "$ANDROID_HOME/sdk/ndk-bundle" "$ANDROID_HOME/ndk"
-		# ln -sf "$ANDROID_HOME/ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin" "$ANDROID_HOME/ndk-toolchain"
-		# ln -sf "$ANDROID_HOME/ndk-toolchain/arm-linux-androideabi-as" "$ANDROID_HOME/ndk-toolchain/arm-linux-as"
-		# ln -sf "$ANDROID_HOME/ndk-toolchain/arm-linux-androideabi-ld" "$ANDROID_HOME/ndk-toolchain/arm-linux-ld"
-
-		# sudo ln -sf "$ANDROID_HOME/ndk-toolchain/arm-linux-androideabi-as" "/usr/bin/arm-linux-androideabi-as"
-		# sudo ln -sf "$ANDROID_HOME/ndk-toolchain/arm-linux-ld"  "/usr/bin/arm-linux-androideabi-ld"
-
-		# aux=$(tail -1 $HOME/.profile)       #tail -1 mostra a última linha do arquivo 
-		# if [ "$aux" != "" ] ; then   # verifica se a última linha é vazia
-		# 		sed  -i '$a\' $HOME/.profile #adiciona uma linha ao fim do arquivo
-		# fi
-
-
-		# profile_file=$HOME/.bashrc
-		# flag_profile_paths=0
-
-		# if [ -e $profile_file ];then 
-		# 	profile_data=$(cat $profile_file)
-		# 	case "$profile_data" in 
-		# 		*'export PATH=$PATH:$HOME/android/'*)
-		# 		flag_profile_paths=1
-		# 		#exit 1
-		# 		;;
-		# 	esac
-		# fi
-
-		# if [ $flag_profile_paths = 0 ] ; then 
-		# 	echo 'export PATH=$PATH:$HOME/android/ndk-toolchain' >> $HOME/.bashrc
-		# 	echo 'export PATH=$PATH:$HOME/android/gradle-4.1/bin' >> $HOME/.bashrc
-		# fi
-
-		# export PATH=$PATH:$HOME/android/ndk-toolchain
-		# export PATH=$PATH:$HOME/android/gradle-4.1/bin
-		# #get fpdlux
-		# changeDirectory $HOME
-		# #codigo para obter o crosscompile
-		# # wget https://github.com/newpascal/fpcupdeluxe/releases/tag/v1.6.1e
-		# # fpcupdeluxe_lnk=($( cat v1.6.1e  | grep x86 | grep linux | sed "s/<a href=\"//g" | sed "s/\"//g")) # processa o arquivo removendo < a href= e = do arquivo 
-		# # i=0
-		# # while [ $i -lt ${#fpcupdeluxe_lnk[*]} ]
-		# # do
-		# # 	echo "i=$i=${fpcupdeluxe_lnk[i]}"
-		# # 	((i++))
-		# # done
-		# # cros_lk=$CROSS_COMPILE_URL${fpcupdeluxe_lnk[0]}
-		# # echo "$cros_lk"
-		# # wget $cros_lk
-
-
-		# #make manual cross comp+i+l+e+
-		# changeDirectory $HOME
-		# mkdir -p $LAZANDROID_HOME
-		# mkdir -p $LAZANDROID_HOME/fpcsrc
-		# changeDirectory $LAZANDROID_HOME/fpcsrc
-		# svn checkout $URL_FPC
-		# #mv $FPC_RELEASE fpcsrc
-		# changeDirectory $FPC_RELEASE
-		# #sudo make clean crossall OS_TARGET=android CPU_TARGET=arm
-		# #sudo make clean crossall  CPU_TARGET=arm OS_TARGET=android OPT="-dFPC_ARMEL" CROSSOPT="-CpARMv6 -CfSoft"
-		# #sudo make crossinstall  CPU_TARGET=arm OS_TARGET=android OPT="-dFPC_ARMEL" CROSSOPT="-CpARMv6 -CfSoft" INSTALL_PREFIX=/usr
-		
-		# #make install TARGET=linux PREFIX_INSTALL=/usr
-		# if [ $flag_new_ubuntu_lts = 0 ]
-		# then
-		# 	sudo make clean crossall crossinstall  CPU_TARGET=arm OS_TARGET=android OPT="-dFPC_ARMHF" SUBARCH="armv7a" INSTALL_PREFIX=/usr		
-		# else
-		# 	# sudo make clean build  install OS_TARGET=linux INSTALL_PREFIX=/usr
-		# 	sudo make clean crossall crossinstall  CPU_TARGET=arm OS_TARGET=android OPT="-dFPC_ARMHF" SUBARCH="armv7a" INSTALL_PREFIX=/usr
-		# 	#sudo cp /tmp/usr/
-		# fi
-
-		# sudo ln -sf $FPC_LIB_PATH/ppcrossarm /usr/bin/ppcrossarm
-		# sudo ln -sf /usr/bin/ppcrossarm /usr/bin/ppcarm
-		# #fi
-
-		# #sudo bash -x $0 cfg-fpc $ANDROID_HOME
-		# #firefox $CROSS_COMPILE_URL
-		# changeDirectory $ANDROID_HOME
-		# svn co https://github.com/jmpessoa/lazandroidmodulewizard.git
-		# ln -sf $ANDROID_HOME/lazandroidmodulewizard.git $ANDROID_HOME/lazandroidmodulewizard
-
-		# changeDirectory $LAZANDROID_HOME
-		# svn co https://svn.freepascal.org/svn/lazarus/tags/lazarus_1_8_4
-		# ln -sf $LAZANDROID_HOME/lazarus_1_8_4 $LAZANDROID_HOME/laz4android
-		# ln -sf $LAZANDROID_HOME/laz4android/lazarus $LAZANDROID_HOME/laz4android/laz4android
-		# changeDirectory $LAZANDROID_HOME/laz4android
-		# make clean all
-
-		# for((i=0;i< ${#LAZBUILD_PARAMETERS[@]};i++))
-		# do
-		# 	./lazbuild ${LAZBUILD_PARAMETERS[i]}
-		# 	if [ $? != 0 ]; then
-		# 		./lazbuild ${LAZBUILD_PARAMETERS[i]}
-		# 	fi
-		# done
-
-		# changeDirectory $ANDROID_HOME
-		# #svn co https://github.com/jmpessoa/lazandroidmodulewizard.git
-		# #ln -sf $ANDROID_HOME/lazandroidmodulewizard.git $ANDROID_HOME/lazandroidmodulewizard
-		# if [ ! -e ~/.local/share/applications ] ; then
-		# 	mkdir ~/.local/share/applications
-		# fi
-		# echo "[Desktop Entry]" > ~/.local/share/applications/laz4android.desktop
-		# echo "Name=laz4android" >>  ~/.local/share/applications/laz4android.desktop
-		# echo "Exec=$LAZANDROID_HOME/laz4android/laz4android --primary-config-path=$HOME/.laz4android" >> ~/.local/share/applications/laz4android.desktop
-		# echo "Icon=$LAZANDROID_HOME/laz4android/images/icons/lazarus_orange.ico" >> ~/.local/share/applications/laz4android.desktop
-		# echo "Type=Application" >> ~/.local/share/applications/laz4android.desktop
-		# echo "Categories=Development;IDE;" >> ~/.local/share/applications/laz4android.desktop
-		# chmod +x ~/.local/share/applications/laz4android.desktop
-		# Laz4AndroidPostConfig
-		# sudo printf 'SUBSYSTEM=="usb", ATTR{idVendor}=="<VENDOR>", MODE="0666", GROUP="plugdev"\n'  | sudo tee /etc/udev/rules.d/51-android.rules
-		# sudo service udev restart
-		# update-menus
-		# zenity --info --text "Suas inforamações\nlaz4android:$LAZANDROID_HOME\nLAMW workspace : $HOME/Dev/lamw_workspace\nAndroid SDK:$HOME/android/sdk\nAndroid NDK:$HOME/android/ndk\nGradle:$ANDROID_HOME/gradle-4.1\n"
-
-
+		mainInstall $2	
 	;;
 
 	"clean-install")
 		CleanOldConfig
 		mainInstall $2
 	;;
+		# "cfg-fpc")
+	# 	if [ "$(whoami)" = "root" ]
+	# 	then
+	# 		echo "executando em mode root"
+	# 		configureFPC $2
+	# 	fi
+	# ;;
 	*)
 		printf "Use:\n\tbash lamw-install.sh clean\n\tbash lamw-install.sh install\n\tbash lamw-install.sh install --use_proxy\n\tbash lamw-install.sh clean-install\n\tbash lamw-install.sh clean-install --use_proxy\n"
 	;;
