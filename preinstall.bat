49c86cad4ed620e10fdd0bae8518ed329ff15f4c
Daniel <oliveira.daniel109@gmail.com>
2018-11-08 14:45:03 -0300

    Fix bar windows

--- preinstall.bat
+++ preinstall.bat
@@ -2,7 +2,7 @@ echo "install apt for windows ..."
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy AllSigned
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Bypass
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" "C:%HoMePath%/Downloads/LAMWAutoRunScripts-master/getMingw.ps1"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" "C:%HoMePath%\Downloads\LAMWAutoRunScripts-master\getMingw.ps1"
 echo "please install mingw wizard"
 C:\mingw-get-setup
 echo "now installing basic Mingw "
