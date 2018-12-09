fa03483d5600608c00bc7664fa3af91a4a689fec
Daniel <oliveira.daniel109@gmail.com>
2018-12-09 02:32:55 -0300

    Fix $ANDROID_HOME

--- getMingw.ps1
+++
@@ -1,8 +0,0 @@
-
-$MINGW_URL="http://c3sl.dl.osdn.jp/mingw/68260/mingw-get-setup.exe"
-$MINGW_PACKAGES="msys-wget-bin  mingw32-base-bin mingw-developer-toolkit-bin msys-base-bin msys-zip-bin "
-$MINGW_PARA=$MINGW_PACKAGES.Split('')
-$MINGW_OPT="--reinstall"
-$client = New-Object System.Net.WebClient
-$path="\mingw-get-setup.exe"
-$client.DownloadFile($MINGW_URL, $path)
