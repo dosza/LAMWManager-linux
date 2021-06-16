999807bda5275335b702bad6fbd289006fd8f84c
Daniel <oliveira.daniel109@gmail.com>
2021-06-15 21:57:25 -0300

    fix LOCAL_ROOT_LAMW=$DEFAULT_ROOT_LAMW
    Prevent unnecesssary and wrog LOCAL_ROOT_LAMW

--- lamw_manager/core/headers/.init_lamw_manager.sh
+++ lamw_manager/core/headers/.init_lamw_manager.sh
@@ -57,11 +57,11 @@ checkisLocalRootLAMWInvalid(){
 }
 setRootLAMW(){
 	if [ ! -e $LAMW_MANAGER_LOCAL_CONFIG_DIR ]; then 
-		if [ "$LOCAL_ROOT_LAMW" = "" ]; then 
+		if [ "$LOCAL_ROOT_LAMW" = "" ] || [ "$LOCAL_ROOT_LAMW" = "$DEFAULT_ROOT_LAMW" ]; then 
 			ROOT_LAMW="$DEFAULT_ROOT_LAMW"
 		else
 
-			if [ -e "$DEFAULT_ROOT_LAMW" ] && [   "$(ls -v $DEFAULT_ROOT_LAMW)" != "" ] ; then 
+			if [ -e "$DEFAULT_ROOT_LAMW" ] && [   "$(ls -v $DEFAULT_ROOT_LAMW)" != "" ]; then 
 				ROOT_LAMW=$DEFAULT_ROOT_LAMW
 				return 
 			fi
@@ -78,6 +78,8 @@ setRootLAMW(){
 			if [ "$LOCAL_ROOT_LAMW" = "" ];then 
 				LOCAL_ROOT_LAMW=$CURRENT_LOCAL_ROOT_LAMW
 			fi
+
+		
 
 			if [ -e "$CURRENT_LOCAL_ROOT_LAMW/lamw4linux/lamw4linux.log" ] ||  [ "$CURRENT_LOCAL_ROOT_LAMW" != "$LOCAL_ROOT_LAMW" ] && [ -e "$CURRENT_LOCAL_ROOT_LAMW" ] && [ ! "$(ls -v "$CURRENT_LOCAL_ROOT_LAMW")" = "" ]; then
 				echo "${VERMELHO}Error: You cannot override ROOT_LAMW, before uninstall LAMW4Linux$NORMAL"
