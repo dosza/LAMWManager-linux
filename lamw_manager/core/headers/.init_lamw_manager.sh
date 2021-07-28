55482e0de39bd0e00be654abec64e68f19082c4f
Daniel <oliveira.daniel109@gmail.com>
2021-07-27 23:00:50 -0300

    Prepare v0.4.1
    Update file versions

--- lamw_manager/core/headers/.init_lamw_manager.sh
+++ lamw_manager/core/headers/.init_lamw_manager.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.4.0
-#Date: 06/12/2021
+#Version: 0.4.1
+#Date: 07/27/2021
 #Description: The ".init_lamw_manager.sh" is part of the core of LAMW Manager. This script check conditions to init LAMW Manager
 #-------------------------------------------------------------------------------------------------#
 
