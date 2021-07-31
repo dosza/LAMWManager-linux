6cbf58e339b170ada5ab01b1ee3ba5d1a51135c0
Daniel <oliveira.daniel109@gmail.com>
2021-07-31 16:48:21 -0300

    Backport Fixes from v0.4.1
    Fix Trap Actions
    Fix ANDROID_SDK_ROOT

--- lamw_manager/core/headers/parser.sh
+++ lamw_manager/core/headers/parser.sh
@@ -65,17 +65,15 @@ initParameters(){
 }
 
 TrapActions(){
-	local sdk_tools_zip=$ANDROID_SDK_ROOT
-	#echo "MAGIC_TRAP_INDEX=$MAGIC_TRAP_INDEX";read
 	local magic_trap=(
 		"$ANT_TAR_FILE" #0 
 		"$ANT_HOME"		#1
 		"$GRADLE_ZIP_FILE" #2
 		"$GRADLE_HOME"   #3
-		"$sdk_tools_zip" #4
-		"$ANDROID_SDK_ROOT" #5
-		"$NDK_ZIP" #6
-		"$NDK_DIR_UNZIP" #7
+		"$SDK_TOOLS_ZIP" #4
+		"$SDK_TOOLS_DIR" #5
+		"$CMD_SDK_TOOLS_ZIP" #6
+		"$CMD_SDK_TOOLS_DIR" #7
 	)
 	
 	if [ "$MAGIC_TRAP_INDEX" != "-1" ]; then
