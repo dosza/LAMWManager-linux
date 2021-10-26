b62aa36135c545896c4e17db6d873fb479c5b8dc
Daniel <oliveira.daniel109@gmail.com>
2021-10-25 22:31:35 -0300

    Update date change files

--- lamw_manager/core/headers/.init_lamw_manager.sh
+++ lamw_manager/core/headers/.init_lamw_manager.sh
@@ -3,7 +3,7 @@
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
 #Version: 0.4.2
-#Date: 07/27/2021
+#Date: 10/26/2021
 #Description: The ".init_lamw_manager.sh" is part of the core of LAMW Manager. This script check conditions to init LAMW Manager
 #-------------------------------------------------------------------------------------------------#
 
--- lamw_manager/core/lamw-mgr.sh
+++ lamw_manager/core/lamw-mgr.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.4.0.3
-#Date: 07/29/2021
+#Version: 0.4.2
+#Date: 10/26/2021
 #Description: The "lamw-install.sh" is part of the core of LAMW Manager. This script configures the development environment for LAMW
 #-------------------------------------------------------------------------------------------------#
 
