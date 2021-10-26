8044184c72ef923ff287e9afe3729b51f6755ad6
Daniel <oliveira.daniel109@gmail.com>
2021-10-25 22:31:35 -0300

    Update date change files

--- lamw_manager/core/cross-builder/cross-builder.sh
+++ lamw_manager/core/cross-builder/cross-builder.sh
@@ -3,7 +3,7 @@
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
 #Version: 0.4.2
-#Date: 07/27/2021
+#Date: 10/26/2021
 #Description:The "cross-builder.sh" is part of the core of LAMW Manager.  This script contains crosscompile compiler generation routines for ARMv7 / AARCH64- Android
 #-------------------------------------------------------------------------------------------------#
 
