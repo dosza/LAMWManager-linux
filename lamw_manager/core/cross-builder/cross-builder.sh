3f47e4f108fd4fe59966896adbc817246017b099
Daniel <oliveira.daniel109@gmail.com>
2021-12-09 01:03:35 -0300

    Prepare v0.4.3
    Fixes: not found lazandroidwizard/package.json

--- lamw_manager/core/cross-builder/cross-builder.sh
+++ lamw_manager/core/cross-builder/cross-builder.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.4.2
-#Date: 10/26/2021
+#Version: 0.4.3
+#Date: 12/09/2021
 #Description:The "cross-builder.sh" is part of the core of LAMW Manager.  This script contains crosscompile compiler generation routines for ARMv7 / AARCH64- Android
 #-------------------------------------------------------------------------------------------------#
 
