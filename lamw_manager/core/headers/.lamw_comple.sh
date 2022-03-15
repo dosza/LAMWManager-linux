496da6652180956b831572a6194842cb1ed59822
dosza <oliveira.daniel109@gmail.com>
2022-03-15 20:34:39 -0300

    add new completation

--- lamw_manager/core/headers/.lamw_comple.sh
+++ lamw_manager/core/headers/.lamw_comple.sh
@@ -8,12 +8,18 @@
 #-------------------------------------------------------------------------------------------------#
 
 
+if [ -e ~/.bashrc ]; then 
+	source ~/.bashrc
+else
+	[ -e  /etc/bash.bashrc ] && source /etc/bash.bashrc
+fi
+
 _lamw_manager_completion() {
 	local cur prev opts
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	opts="--help  --sdkmanager --reset --reset-aapis --update-lamw --use_proxy --server --port "
+	opts="--help  --minimal --port --reinstall --reset --reset-aapis --sdkmanager  --server --update-lamw  --use_proxy"
 
 	if [[ ${cur} == -* ]] ; then
 		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
@@ -21,8 +27,18 @@ _lamw_manager_completion() {
 	fi
 }
 
+_sdkmanager_completion(){
+	local cur prev opts
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	opts="--channel= --help --include_obsolete --install --licenses --list --list_installed --no_https --package --package_combination --proxy= --proxy_host= --proxy_port= --sdk_root= --uninstall --update --verbose --version"
 
+	if [[ ${cur} == -* ]] ; then
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		return 0
+	fi
+}
 complete -F _lamw_manager_completion lamw_manager
-complete -F _lamw_manager_completion lamw-manager
 complete -F _lamw_manager_completion ./lamw_manager
-complete -F _lamw_manager_completion ./lamw-manager
+complete -F  _sdkmanager_completion sdkmanager
