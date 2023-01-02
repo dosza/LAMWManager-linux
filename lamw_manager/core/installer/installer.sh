f6fd76cd6e3c1a9253571ae97ee36ef16051db33
dosza <oliveira.daniel109@gmail.com>
2023-01-02 17:40:36 -0300

    enable multi-thread build lazarus

--- lamw_manager/core/installer/installer.sh
+++ lamw_manager/core/installer/installer.sh
@@ -606,7 +606,7 @@ BuildLazarusIDE(){
 	local error_build_lazarus_msg="${VERMELHO}Fatal error:${NORMAL}Fails in build Lazarus!!"
 	
 	local make_opts=( 
-		"clean all" "PP=${FPC_TRUNK_LIB_PATH}/ppcx64" "FPC_VERSION=$_FPC_TRUNK_VERSION" )
+		"clean all" "PP=${FPC_TRUNK_LIB_PATH}/ppcx64" "FPC_VERSION=$_FPC_TRUNK_VERSION"  "FPMAKEOPT=-T${CPU_COUNT}")
 	local build_msg="Please wait, starting build Lazarus to ${NEGRITO}x86_64/Linux${NORMAL}.............."
 	local sucess_filler="$(getCurrentSucessFiller 1 x86_64/Linux)"
 	changeDirectory $LAMW_IDE_HOME
