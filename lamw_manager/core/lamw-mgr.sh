577e3d23d9193e3737ee842c53a07243b0e138f2
dosza <oliveira.daniel109@gmail.com>
2023-01-05 01:25:18 -0300

    update version from 0.5.2 to 0.5.3

--- lamw_manager/core/lamw-mgr.sh
+++ lamw_manager/core/lamw-mgr.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.5.2
-#Date: 12/06/2022
+#Version: 0.5.3
+#Date: 01/05/2023
 #Description: The "lamw-install.sh" is part of the core of LAMW Manager. This script configures the development environment for LAMW
 #-------------------------------------------------------------------------------------------------#
 
