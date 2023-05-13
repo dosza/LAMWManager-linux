be97e1d761c292e53becaa0ec9c5dc153ee9135b
dosza <oliveira.daniel109@gmail.com>
2023-05-13 00:32:38 -0300

    prepare v0.5.4

--- lamw_manager/core/headers/.init_lamw_manager.sh
+++ lamw_manager/core/headers/.init_lamw_manager.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.5.3
-#Date: 01/05/2023
+#Version: 0.5.4
+#Date: 05/13/2023
 #Description: The ".init_lamw_manager.sh" is part of the core of LAMW Manager. This script check conditions to init LAMW Manager
 #-------------------------------------------------------------------------------------------------#
 
