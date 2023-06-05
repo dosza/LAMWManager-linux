aa6a1741805062f0a04179d66bcdfc3e07cb5d63
dosza <oliveira.daniel109@gmail.com>
2023-06-04 23:50:24 -0300

    prepare v0.5.6

--- lamw_manager/core/headers/.lamw_comple.sh
+++ lamw_manager/core/headers/.lamw_comple.sh
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.5.5
+#Version: 0.5.6
 #Description: This script contains routines for completing LAMW Manager arguments.
 #Ref:https://www.vivaolinux.com.br/dica/Shell-script-autocompletion-Como-implementar
 #-------------------------------------------------------------------------------------------------#
