4fd254a2d02b406dddd90a2ddddc8461e856a6b1
dosza <oliveira.daniel109@gmail.com>
2023-06-04 23:50:24 -0300

    prepare v0.5.6

--- lamw_manager/core/lamw-mgr.sh
+++ lamw_manager/core/lamw-mgr.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.5.5
-#Date: 05/29/2023
+#Version: 0.5.6
+#Date: 06/04/2023
 #Description: The "lamw-install.sh" is part of the core of LAMW Manager. This script configures the development environment for LAMW
 #-------------------------------------------------------------------------------------------------#
 
