49a04730e860b11a44f9903ec25114cec75bdc1c
dosza <oliveira.daniel109@gmail.com>
2024-01-01 08:12:29 -0300

    prepare v0.5.9.2

--- lamw_manager/core/cross-builder/cross-builder.sh
+++ lamw_manager/core/cross-builder/cross-builder.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.5.9.1
-#Date: 12/27/2023
+#Version: 0.5.9.2
+#Date: 01/01/2024
 #Description:The "cross-builder.sh" is part of the core of LAMW Manager.  This script contains crosscompile compiler generation routines for ARMv7 / AARCH64- Android
 #-------------------------------------------------------------------------------------------------#
 
