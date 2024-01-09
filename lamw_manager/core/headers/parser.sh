78309c860de23227dda352baec4e3c510578ce95
dosza <oliveira.daniel109@gmail.com>
2024-01-09 19:27:50 -0300

    remove support to ENTER=1 flag

--- lamw_manager/core/headers/parser.sh
+++ lamw_manager/core/headers/parser.sh
@@ -215,7 +215,6 @@ testConnectionInternetOnDemand(){
 parseFlags(){
 	CheckFlags DEBUG "DEBUG=1" 1
     CheckFlags USE_PKEXEC "PKEXEC=1" 1
-    CheckFlags ENTER_TO_EXIT "ENTER=1" 1
     CheckFlags NOBLINK 'NOBLINK=1' 1
 }
 
