e7635932b3f02078c6143b8f270552fa256f1c9a
dosza <oliveira.daniel109@gmail.com>
2024-01-12 19:25:16 -0300

    rewrite preinstall actions

--- lamw_manager/core/installer/preinstall.sh
+++ lamw_manager/core/installer/preinstall.sh
@@ -3,35 +3,40 @@
 GetLocalJavaHome(){
 	local lamw4linux_env="$LAMW4LINUX_HOME/etc/environment"
 	if [ -e $lamw4linux_env ]; then
-		export JAVA_HOME=$(grep '^export JAVA_HOME' "$lamw4linux_env" | sed 's|export JAVA_HOME=||g')
+		LOCAL_JAVA_HOME=$(grep '^export JAVA_HOME' "$lamw4linux_env" | sed 's|export JAVA_HOME=||g')
 	fi	
 }
 StopGradleDaemon(){
+	
+	if [ ! -e "$LAMW_INSTALL_LOG" ] ; then
+		return 
+	fi
+	
+	local gradle_version="$(grep '^GRADLE_VERSION' $LAMW_INSTALL_LOG |
+		 	sed 's/GRADLE_VERSION=//g')"
+
 	local sucess_filler="stopping Gradle Daemons"
 
-	if [ -e "$LAMW_INSTALL_LOG" ] ; then
-		local gradle_version="$(grep '^GRADLE_VERSION' $LAMW_INSTALL_LOG |
-		 	sed 's/GRADLE_VERSION=//g'
-		)"
-
-		local gradle_path="$ROOT_LAMW/gradle-$gradle_version"
-		GetLocalJavaHome
-		LAMW_INSTALL_LOG_CHANGES['INITIAL']=$gradle_version
-	fi
-
-	if [ "$gradle_path" != "" ] && [ -e "$gradle_path" ]; then 
+	local gradle_path="$ROOT_LAMW/gradle-$gradle_version"
 		
-		if ps  -e -o command | grep "^${JAVA_HOME}.*$ROOT_LAMW/gradle" -q ; then
-			
-			echo "${sucess_filler^} ..."
-			startProgressBar
-			
-			if $gradle_path/bin/gradle --stop -q &>/dev/null; then
-				stopAsSuccessProgressBar
-			else
-				stopProgressBarAsFail
-			fi
 
-		fi
+	if [ "$gradle_path" = "" ]|| [ ! -e "$gradle_path" ]; then 
+		return 
 	fi
+	
+	GetLocalJavaHome
+	
+	if ! ps  -e -o command | grep "^${LOCAL_JAVA_HOME}.*$ROOT_LAMW/gradle" -q ; then
+		return
+	fi
+			
+	echo "${sucess_filler^} ..."
+	startProgressBar
+	
+	if env JAVA_HOME=$LOCAL_JAVA_HOME $gradle_path/bin/gradle --stop -q &>/dev/null; then
+		stopAsSuccessProgressBar
+	else
+		stopProgressBarAsFail
+	fi
+
 }
