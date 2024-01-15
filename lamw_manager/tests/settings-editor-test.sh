084814780b0aecefd3eedb4f7a9c7dfdb0b9b787
dosza <oliveira.daniel109@gmail.com>
2024-01-15 13:08:42 -0300

    add tests to create/update $LAMW_IDE_HOME_CFG

--- lamw_manager/tests/settings-editor-test.sh
+++ lamw_manager/tests/settings-editor-test.sh
@@ -35,5 +35,30 @@ testAddLAMWtoStartMenu(){
 	ls -la $LAMW_MENU_ITEM_PATH &>/dev/null
 	assertTrue $?
 }
+
+testInitLAMw4LinuxConfig(){
+	
+	local env_opts_node="//CONFIG/EnvironmentOptions"
+	local lazarus_env_cfg_path="$LAMW_IDE_HOME_CFG/environmentoptions.xml"
+	
+	mkdir -p $LAMW_IDE_HOME/tools/install/
+	echo "echo $LAZARUS_STABLE_VERSION" >$LAMW_IDE_HOME/tools/install/get_lazarus_version.sh
+	chmod +x $LAMW_IDE_HOME/tools/install/get_lazarus_version.sh
+	rm -rf $lazarus_env_cfg_path &>/dev/null
+	
+	initLAMw4LinuxConfig
+	assertEquals "[Create default $lazarus_env_cfg_path]" "$LAZARUS_STABLE_VERSION" "$( getNodeAttrXML "$env_opts_node/Version/@Lazarus"  $lazarus_env_cfg_path)"
+	
+	sed -i "s/$LAZARUS_STABLE_VERSION/2.0.6/g" $lazarus_env_cfg_path
+	initLAMw4LinuxConfig
+	assertEquals '[Update lazarus version node]' "$LAZARUS_STABLE_VERSION" "$( getNodeAttrXML $env_opts_node/Version/@Lazarus  $lazarus_env_cfg_path)"
+	
+	sed -i "/FppkgConfigFile/d" $lazarus_env_cfg_path
+	initLAMw4LinuxConfig
+	grep 'FppkgConfigFile' $lazarus_env_cfg_path -q
+	assertTrue '[Insert FppkgConfigFile node]' $?
+
+}
+
 . $(which shunit2)
 rm -rf $HOME
