5ed4997b0522ed21c1e923b41cd7cd533bde5c76
dosza <oliveira.daniel109@gmail.com>
2024-01-15 12:39:27 -0300

    add settings-editor.sh basic tests

--- lamw_manager/tests/settings-editor-test.sh
+++ lamw_manager/tests/settings-editor-test.sh
@@ -9,5 +9,31 @@ testInitLAMWUserConfig(){
 	assertTrue $?
 }
 
+
+testInitRootLAMW(){
+	initROOT_LAMW
+	ls -ls $ROOT_LAMW &>/dev/null
+	assertTrue $?
+}
+
+testCreateLazarusEnvCfgFile(){
+	local lazarus_env_cfg_path="$LAMW_IDE_HOME_CFG/environmentoptions.xml"
+	createLazarusEnvCfgFile
+	ls -la $lazarus_env_cfg_path &>/dev/null
+	assertTrue $?
+}
+
+testAddLAMWtoStartMenu(){
+	initROOT_LAMW
+	mkdir $LAMW_IDE_HOME/install -p
+	cd $LAMW_IDE_HOME/install
+	wget -qc "https://raw.githubusercontent.com/alrieckert/lazarus/master/install/lazarus.desktop"
+	wget -qc "https://raw.githubusercontent.com/alrieckert/lazarus/master/install/lazarus-mime.xml" 
+	cd ..
+	AddLAMWtoStartMenu
+
+	ls -la $LAMW_MENU_ITEM_PATH &>/dev/null
+	assertTrue $?
+}
 . $(which shunit2)
 rm -rf $HOME
