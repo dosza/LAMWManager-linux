8b44d7fae41120a7339a011471e616c78b837c7c
dosza <oliveira.daniel109@gmail.com>
2024-01-15 10:35:34 -0300

    remove repeat code

--- lamw_manager/tests/parse-tests.sh
+++ lamw_manager/tests/parse-tests.sh
@@ -1,28 +1,6 @@
-if [ -e "$PWD/lamw_manager" ];then 
-	lamw_manager_script="$(realpath ./lamw_manager)"
-else 	
-	lamw_manager_script=$(realpath ../lamw_manager)	
-fi
-
-export HOME=$(mktemp -dt  danny.XXXXXXXXXXX)
-
-LAMW_MANAGER_MODULES_PATH=$(dirname $lamw_manager_script)/core
-source "$LAMW_MANAGER_MODULES_PATH/headers/.index"
-source "$LAMW_MANAGER_MODULES_PATH/headers/common-shell.sh"
-source "$LAMW_MANAGER_MODULES_PATH/headers/.init_lamw_manager.sh"
-source "$LAMW_MANAGER_MODULES_PATH/headers/lamw4linux_env.sh"
-source "$LAMW_MANAGER_MODULES_PATH/headers/lamw_headers"
-source "$LAMW_MANAGER_MODULES_PATH/headers/parser.sh"
-source "$LAMW_MANAGER_MODULES_PATH/installer/services.sh"
-source "$LAMW_MANAGER_MODULES_PATH/installer/distro-overrides.sh"
-source "$LAMW_MANAGER_MODULES_PATH/installer/configure.sh"
-source "$LAMW_MANAGER_MODULES_PATH/installer/installer.sh"
-source "$LAMW_MANAGER_MODULES_PATH/installer/preinstall.sh"
-source "$LAMW_MANAGER_MODULES_PATH/installer/postinstall.sh"
-source "$LAMW_MANAGER_MODULES_PATH/settings-editor/lamw-settings-editor.sh"
-source "$LAMW_MANAGER_MODULES_PATH/cross-builder/cross-builder.sh"
-source "$LAMW_MANAGER_MODULES_PATH/components/progress-bar.sh"
-
+#!/bin/bash
+TEST_MODULES_PATH=$(dirname $(realpath $0))
+source $TEST_MODULES_PATH/tests-header
 export ROOT_LAMW=~/LAMW
 echo $HOME
 testfindProxyOpt(){
