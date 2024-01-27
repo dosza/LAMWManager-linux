9fe2dc696739dcdd1d9c524590271b50a4ebc7bd
dosza <oliveira.daniel109@gmail.com>
2024-01-27 10:15:03 -0300

    prepare v0.6.2

--- lamw_manager/core/settings-editor/lamw-settings-editor.sh
+++ lamw_manager/core/settings-editor/lamw-settings-editor.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (mater-alma)
 #Course: Science Computer
-#Version: 0.6.1
-#Date: 01/23/2024
+#Version: 0.6.2
+#Date: 01/27/2024
 #Description: The "lamw-manager-settings-editor.sh" is part of the core of LAMW Manager. Responsible for managing LAMW Manager / LAMW configuration files..
 #-----------------------------------------------------------------------f--------------------------#
 
