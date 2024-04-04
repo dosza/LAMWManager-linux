722bcbb442c98ce544e8dbefe2cc935b76145dbc
dosza <oliveira.daniel109@gmail.com>
2024-04-04 12:26:02 -0300

    prepare v0.6.6

--- lamw_manager/core/installer/installer.sh
+++ lamw_manager/core/installer/installer.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.6.5
-#Date: 02/11/2024
+#Version: 0.6.6
+#Date: 04/04/2024
 #Description: "installer.sh" is part of the core of LAMW Manager. Contains routines for installing LAMW development environment
 #-------------------------------------------------------------------------------------------------#
 
--- lamw_manager/core/settings-editor/lamw-settings-editor.sh
+++ lamw_manager/core/settings-editor/lamw-settings-editor.sh
@@ -2,8 +2,8 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (mater-alma)
 #Course: Science Computer
-#Version: 0.6.5
-#Date: 02/11/2024
+#Version: 0.6.6
+#Date: 04/04/2024
 #Description: The "lamw-manager-settings-editor.sh" is part of the core of LAMW Manager. Responsible for managing LAMW Manager / LAMW configuration files..
 #-----------------------------------------------------------------------f--------------------------#
 
