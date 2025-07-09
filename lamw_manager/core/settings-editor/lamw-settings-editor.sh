c7cfa2e2d237bcc0514d616a0f39a9dc3ac0f9eb
dosza <sza.oliveiradaniel@gmail.com>
2025-07-09 19:56:37 -0300

    :bug: fixes: fixes remove unnecessary fppkgconfigFile node

--- lamw_manager/core/settings-editor/lamw-settings-editor.sh
+++ lamw_manager/core/settings-editor/lamw-settings-editor.sh
@@ -731,11 +731,6 @@ updateNodeAttrXML(){
 }
 
 fixesFppkgXmlNode(){
-	local fppkg_count="$(grep FppkgConfigFile $lazarus_env_cfg_path -c)"
-	
-	if grep "FppkgConfigFile\sValue=".*"\sValue"   -q "$lazarus_env_cfg_path"  || [ $fppkg_count -ge 2 ]; then 
-		sed -i "/<FppkgConfigFile.*/d" "$lazarus_env_cfg_path"
-	fi
 
 	if ! grep 'FppkgConfigFile' $lazarus_env_cfg_path > /dev/null ; then  #insert fppkg_config ref: https://stackoverflow.com/questions/7837879/xmlstarlet-update-an-attribute
 		xmlstarlet ed  --inplace -s "$env_opts_node" -t elem -n "FppkgConfigFile" -v "" -i $fppkg_cfg_node -t attr -n "Value" -v "$FPPKG_TRUNK_CFG_PATH" $lazarus_env_cfg_path
