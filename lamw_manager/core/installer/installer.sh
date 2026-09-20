3eabb2f05a78aa58ff11246389faf48ee0b9c560
dosza <oliveira.daniel109@gmail.com>
2026-09-19 22:28:04 -0300

    :bug: fixes: minor fixes

--- lamw_manager/core/installer/installer.sh
+++ lamw_manager/core/installer/installer.sh
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------------------------------------#
 #Universidade federal de Mato Grosso (Alma Mater)
 #Course: Science Computer
-#Version: 0.6.14
+#Version: 0.6.15
 #Date: 09/19/2026
 #Description: "installer.sh" is part of the core of LAMW Manager. Contains routines for installing LAMW development environment
 #-------------------------------------------------------------------------------------------------#
